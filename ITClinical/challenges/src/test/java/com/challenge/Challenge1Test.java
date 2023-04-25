4286274bf0c2493f0f0c1ba7fc93aeeb809cc645
Victor Cracel <vcmessner@hotmail.com.br>
2023-04-25 14:37:01 -0300

    Created String Assert Test

--- ITClinical/challenges/src/test/java/com/challenge/Challenge1Test.java
+++ ITClinical/challenges/src/test/java/com/challenge/Challenge1Test.java
@@ -1,6 +1,5 @@
 package com.challenge;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Random;
@@ -11,11 +10,10 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import com.challenge.Support.StringGenerationSupport;
-import com.challenge.Support.StringVerificationSupport;
+
 
 public class Challenge1Test {
     
-    private StringVerificationSupport verificator = new StringVerificationSupport();
 	private StringGenerationSupport GenStr = new StringGenerationSupport();
     
     @Test
@@ -61,53 +59,6 @@ public class Challenge1Test {
         int N= -ran.nextInt(text.length());
         Challenge1 myChallenge1 =new Challenge1(text, N);
         assertTrue(myChallenge1.Solve().equals(""));
-    }
-
-
-    @RepeatedTest(1000)
-    void test_Assert_Number_as_Integer(){
-		String output = GenStr.Create_Random_Number_String(50,1);
-		assertFalse(verificator.Validate_isAlpha(output));
-		assertFalse(verificator.Validate_isLowerCaseAlpha(output));
-		assertFalse(verificator.Validate_isUpperCaseAlpha(output));
-		assertTrue(verificator.Validate_is_Num(output));
-	}
-
-    @RepeatedTest(1000)
-    void test_Assert_String_as_LowerCaseAlpha(){
-        String output = GenStr.Create_Random_Alpha_String(0, 1);
-		assertTrue(verificator.Validate_isAlpha(output));
-		assertTrue(verificator.Validate_isLowerCaseAlpha(output));
-		assertFalse(verificator.Validate_isUpperCaseAlpha(output));
-		assertFalse(verificator.Validate_is_Num(output));
-		assertFalse(verificator.Validate_is_Pos_Num(output));
-		assertFalse(verificator.Validate_isUpperCaseAlNum(output));
-	}
-
-
-    @RepeatedTest(1000)
-    void test_Assert_String_as_UpperCaseAlpha(){
-		String output = GenStr.Create_Random_Alpha_String(100, 1);
-		assertTrue(verificator.Validate_isAlpha(output));
-		assertTrue(verificator.Validate_isUpperCaseAlpha(output));
-		assertFalse(verificator.Validate_isLowerCaseAlpha(output));
-		assertFalse(verificator.Validate_is_Num(output));
-		assertFalse(verificator.Validate_is_Pos_Num(output));
-		assertFalse(verificator.Validate_isLowerCaseAlNum(output));
-	}
-
-	@RepeatedTest(1000)
-    void test_Assert_String_as_LowerCaseAlphaNum(){
-        String output = GenStr.Create_Random_AlphaNum_String(0,1);
-		assertFalse(verificator.Validate_isUpperCaseAlpha(output));
-        assertTrue(verificator.Validate_isLowerCaseAlNum(output));
-	}
-
-    @RepeatedTest(1000)
-    void test_Assert_String_as_UpperCaseAlphaNum(){
-        String output = GenStr.Create_Random_AlphaNum_String(100,1);
-		assertFalse(verificator.Validate_isLowerCaseAlpha(output));
-		assertTrue(verificator.Validate_isUpperCaseAlNum(output)); 
     }
 
     @RepeatedTest(1000)
--- ITClinical/challenges/src/test/java/com/challenge/Challenge3Test.java
+++ ITClinical/challenges/src/test/java/com/challenge/Challenge3Test.java
@@ -82,54 +82,6 @@ public class Challenge3Test {
         assertTrue(myChallenge3.Solve().equals(""));
     }
 
-
-
-    @RepeatedTest(100)
-    void test_Assert_Number_as_Integer(){
-		String output = GenStr.Create_Random_Number_String(50,1);
-		assertFalse(verificator.Validate_isAlpha(output));
-		assertFalse(verificator.Validate_isLowerCaseAlpha(output));
-		assertFalse(verificator.Validate_isUpperCaseAlpha(output));
-		assertTrue(verificator.Validate_is_Num(output));
-	}
-
-    @RepeatedTest(100)
-    void test_Assert_String_as_LowerCaseAlpha(){
-        String output = GenStr.Create_Random_Alpha_String(0, 1);
-		assertTrue(verificator.Validate_isAlpha(output));
-		assertTrue(verificator.Validate_isLowerCaseAlpha(output));
-		assertFalse(verificator.Validate_isUpperCaseAlpha(output));
-		assertFalse(verificator.Validate_is_Num(output));
-		assertFalse(verificator.Validate_is_Pos_Num(output));
-		assertFalse(verificator.Validate_isUpperCaseAlNum(output));
-	}
-
-
-    @RepeatedTest(100)
-    void test_Assert_String_as_UpperCaseAlpha(){
-		String output = GenStr.Create_Random_Alpha_String(100, 1);
-		assertTrue(verificator.Validate_isAlpha(output));
-		assertTrue(verificator.Validate_isUpperCaseAlpha(output));
-		assertFalse(verificator.Validate_isLowerCaseAlpha(output));
-		assertFalse(verificator.Validate_is_Num(output));
-		assertFalse(verificator.Validate_is_Pos_Num(output));
-		assertFalse(verificator.Validate_isLowerCaseAlNum(output));
-	}
-
-	@RepeatedTest(100)
-    void test_Assert_String_as_LowerCaseAlphaNum(){
-        String output = GenStr.Create_Random_AlphaNum_String(0,1);
-		assertFalse(verificator.Validate_isUpperCaseAlpha(output));
-        assertTrue(verificator.Validate_isLowerCaseAlNum(output));
-	}
-
-    @RepeatedTest(100)
-    void test_Assert_String_as_UpperCaseAlphaNum(){
-        String output = GenStr.Create_Random_AlphaNum_String(100,1);
-		assertFalse(verificator.Validate_isLowerCaseAlpha(output));
-		assertTrue(verificator.Validate_isUpperCaseAlNum(output)); 
-    }
-
     @RepeatedTest(100)
     void test_Solve_Numeric_Input_Challenges(){
 		String text = GenStr.Create_Random_Number_String(50, 10);
