7ca0ec670f84d1ecd1325173db5cf188f40c2aee
Victor Cracel <vcmessner@hotmail.com.br>
2023-04-25 10:39:53 -0300

    Testing Getters/Setters/Constructor

--- ITClinical/challenges/src/main/java/com/challenge/Challenge1.java
+++ ITClinical/challenges/src/main/java/com/challenge/Challenge1.java
@@ -1,10 +1,36 @@
 package com.challenge;
 
 public class Challenge1 {
-    public String Solve(int value) {
-        String result = "Challenge1";
-        return result;
+    String MyText="";
+    int N=0;
+
+    public Challenge1(String myText, int n) {
+        MyText = myText;
+        N = n;
     }
+    
+    public String getMyText() {
+        return MyText;
+    }
+
+    public void setMyText(String myText) {
+        MyText = myText;
+    }
+    
+    public int getN() {
+        return N;
+    }
+
+    public void setN(int n) {
+        N = n;
+    }
+
+
+    public String Solve(String text,int value) {
+        return "";
+    }
+
+    
 
 
     public static void main(String[] args){
--- ITClinical/challenges/src/test/java/com/challenge/Challenge1Test.java
+++ ITClinical/challenges/src/test/java/com/challenge/Challenge1Test.java
@@ -5,12 +5,18 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class Challenge1Test {
-    Challenge1 myChallenge1 = new Challenge1();
     
     @Test
-    void test_my_Challenge(){
-        assertTrue(myChallenge1.Solve(0).equals("Challenge1"));
-
+    void test_input_Challenge(){
+        String text = "ITCLiNicAl";
+        int N = 1;
+        Challenge1 myChallenge1 =new Challenge1(text, N);
+        assertTrue(myChallenge1.getMyText().equals(text));
+        assertTrue(myChallenge1.getN()==N);
     }
+
+
+
+
     
 }
